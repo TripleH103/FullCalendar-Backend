e2a6f8b174de5433b7877b8d4dcf2dff8fede600
TriplehH103 <79268677@qq.com>
2023-05-20 17:34:01 +0800

    tasksController&taskRoutes updata

--- src/controllers/tasksController.ts
+++ src/controllers/tasksController.ts
@@ -50,3 +50,48 @@ export const getTasks: RequestHandler = async (req, res, next) => {
         next (error)
     }
 };
+
+interface filterParams {
+    'resources.office'?:string;
+    'resources.pokemon'?:string
+  }
+export const getFilterTasks: RequestHandler<unknown,unknown,unknown,filterParams> = async (req, res, next) => {
+    try {
+        const officeQuery = req.query['resources.office'];
+        const pokemonQuery = req.query['resources.pokemon'];
+
+        let officeTasks;
+        if (officeQuery) {
+            officeTasks = await TaskModel.find({'resources.office': officeQuery}).exec();
+            if(!officeTasks) {
+                throw createHttpError(404, 'nothing found in the DB')
+            }
+        }
+
+        let pokemonTasks;
+        if (pokemonQuery) {
+            pokemonTasks = await TaskModel.find({'resources.pokemon': pokemonQuery}).exec();
+            if(!pokemonTasks) {
+                throw createHttpError(404, 'nothing found in the DB')
+            }
+        }
+
+        if (officeTasks && pokemonTasks) {
+            res.status(200).json({officeTasks, pokemonTasks});
+            return;
+        }
+
+        if (officeTasks) {
+            res.status(200).json(officeTasks);
+            return;
+        }
+
+        if (pokemonTasks) {
+            res.status(200).json(pokemonTasks);
+            return;
+        }
+        throw createHttpError(400, 'Invalid query parameters');
+    } catch (error) {
+        next (error)
+    }
+}
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -3,6 +3,7 @@ import * as taskController from "../controllers/tasksController";
 
 const router = express.Router();
 
+router.get("/find", taskController.getFilterTasks);
 router.get("/", taskController.getTasks);
 router.post("/new-task", taskController.createTasks);
 
